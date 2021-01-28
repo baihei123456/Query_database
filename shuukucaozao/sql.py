91e23ca9969e9aa71a5f36c3913a69ca8ec4d6b8
huminghuan001 <21846582@qq.com>
2021-01-28 15:05:08 +0800

    '操作数据库优化'

--- shuukucaozao/sql.py
+++ shuukucaozao/sql.py
@@ -1,23 +1,25 @@
 # -*- coding: UTF-8 -*-
 #导入包
 import pymysql
-#创建连接对象
-conn=pymysql.connect(host="127.0.0.1",
-                     port=3306,
-                     user="root",
-                     password="",
-                     database="test",
-                     charset="utf8")
-#获取游标，目的时执行sql语句
-cursor=conn.cursor()
-#准备sql
-sql="SELECT * from student;"
-#执行sql语句
-cursor.execute(sql)
-#获取查询的结果
-result=cursor.fetchall()
-print(result)
-#关闭游标
-cursor.close()
-#关闭连接
-conn.close()
+if __name__ == '__main__':
+
+    #创建连接对象
+    conn=pymysql.connect(host="127.0.0.1",
+                         port=3306,
+                         user="root",
+                         password="",
+                         database="test",
+                         charset="utf8")
+    #获取游标，目的时执行sql语句
+    cursor=conn.cursor()
+    #准备sql
+    sql="SELECT * from student;"
+    #执行sql语句
+    cursor.execute(sql)
+    #获取查询的结果
+    result=cursor.fetchall()
+    print(result)
+    #关闭游标
+    cursor.close()
+    #关闭连接
+    conn.close()
